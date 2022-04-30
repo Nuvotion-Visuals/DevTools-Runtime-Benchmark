6cbacc52db02696692c32134bf9baeceaa1864e6
tom-leamon <leamon@outlook.com>
2022-04-29 23:17:49 -0500

    Fix incorrect filtering of valid benchmark directories

--- report-server.js
+++ report-server.js
@@ -32,7 +32,7 @@ app.get('/', function (req, res, next) {
   const directories = fs.readdirSync(__dirname + '\\benchmark-result\\', { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name)
-    .filter(benchmark => fs.existsSync(`/benchmark-result/${benchmark}`))
+    .filter(benchmark => fs.existsSync(__dirname + `/benchmark-result/${benchmark}/${benchmark}.json`))
 
   const benchmarkInfo = {}
 
